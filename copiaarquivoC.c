c43fe7498ddd22bf83d293f1e9311fb690ee5ad5
luisgkingeski <44435379+luisgkingeski@users.noreply.github.com>
2020-12-11 15:02:06 -0300

    Add files via upload

---
+++ copiaarquivoC.c
@@ -0,0 +1,59 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+double converteBytes(int tam){
+double t;
+	switch(tam){	
+		case 1:t = 1;break;
+		case 2:t = 1024; break;
+		case 3:t = 1024 * 1024; break;
+		case 4:t = 1024 * 1024 * 1024; break;
+		case 5:t = (double)( 1024 * 1024 * 1024 ) * 5; break;
+		default: t = tam; break; 
+	}
+	return t;
+}
+
+void menu(){
+	printf("Digite a quantidade de bytes para seu arquivo:\n");
+	printf("[1] 1B\n");
+	printf("[2] 1KB\n");
+	printf("[3] 1MB\n");
+	printf("[4] 1GB\n");
+	printf("[5] 5GB\n");
+	printf("[] Definida pelo usuario\n");
+}
+double tamArquivo(){
+	double tam;
+	menu();
+	scanf("%lf",&tam);	
+	return tam;
+}
+void escreverArquivo(double tam){
+    int nread,i=0;
+    FILE *out;
+    struct timeval tempo1, tempo2;
+    struct timezone tzp;
+    double tempo;
+	out = fopen("file.out","w");
+	gettimeofday(&tempo1,&tzp);
+	while(i<tam){
+		fwrite("X",1,1,out);
+		i++;
+	}
+	gettimeofday(&tempo2,&tzp);
+        tempo = (double)(tempo2.tv_sec - tempo1.tv_sec) + (((double)(tempo2.tv_usec - tempo1.tv_usec))/1000000);
+	printf("\nTempo decorrido: %f\n",tempo);
+}
+
+
+int main()
+{	double tamanhoArquivo;
+	tamanhoArquivo = tamArquivo();
+	escreverArquivo(converteBytes(tamanhoArquivo));
+	return(0);
+}
+
